ad03d710031546d555f73c3687c8f7a8199d7e8e
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-06-21 07:55:45 -0500

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_231359) do
+ActiveRecord::Schema.define(version: 2021_06_21_125105) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,7 +40,8 @@ ActiveRecord::Schema.define(version: 2021_06_20_231359) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "images", force: :cascade do |t|
+  create_table "pictures", force: :cascade do |t|
+    t.string "image_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

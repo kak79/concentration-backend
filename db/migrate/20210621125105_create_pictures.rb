ba496f89679641a2b63cc23aadd27e17ed784add
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-06-28 12:08:33 -0500

    dropped the db

--- db/migrate/20210621125105_create_pictures.rb
+++
@@ -1,9 +0,0 @@
-class CreatePictures < ActiveRecord::Migration[6.1]
-  def change
-    create_table :pictures do |t|
-      t.string :image_name
-
-      t.timestamps
-    end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+pictures = Picture.create(
+  [
+    {
+      image_name: 'Boar'
+    },
+    {
+      image_name: 'cardBack'
+    },
+    {
+      image_name: 'Cat'
+    },
+    {
+      image_name: 'Cow and Calf'
+    },
+    {
+      image_name: 'Deer'
+    },
+    { 
+      image_name: 'Ducks'
+    },
+    {
+      image_name: 'Goose'
+    },
+    {
+      image_name: 'Horse'
+    },
+    {
+      image_name: 'Piglet'
+    },
+    {
+      image_name: 'Puppy'
+    },
+    {
+      image_name: 'Rooster'
+    },
+    {
+      image_name: 'White Moose'
+    },
+    {
+      image_name: 'Wolf'
+    }
+  ]
+)
